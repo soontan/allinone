bd2d58d2f1b40f077b1765173010c8e6237e00e2
soontan <luandsun@foxmail.com>
2016-02-29 18:55:44 +0800

    day10:html1.php

---
+++ 1.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<!-- comtents not for read -->
+<!-- head information doesn't appear in the page -->
+<head>
+	<!-- only one -->
+	<title>all in one</title>
+	<!-- only one -->
+	<base href="http://localhost" />
+	<link rel="stylesheet" type="text/css" href="demo.css" />
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="keywords" content="all,one" />
+	<meta name="description" content="from the very beginning" />
+
+<style>
+	body{
+		background:green;
+		color:blue;
+		background-color:black;
+		background-color:yellow;
+		background-color:blue;
+		/*grey is highlighted in sublime ,but why*/
+		background-color:grey;
+		background-color:#cccccc;
+		background-color:#BEBEBE;
+	}
+	
+</style>
+</head>
+
+<body>
+	<a href="1.php">1.php in default dir</a><br />
+	<a href="1.php#sss" target="_blank">h7</a>
+	<h1>&lt;h1&gt;</h1>
+	<h2>&lt;h2&gt;</h2>
+	<h3>&lt;h3&gt;</h3>
+	<h4>&lt;h4&gt;</h4>
+	<h5>&lt;h5&gt;</h5>
+	<h6>&lt;h6&gt;</h6>
+<!-- there's no h7 -->
+	<a name="sss"><h7>&lt;h7&gt;</h7>
+	<h7>&lt;h7&gt;</h7></a>
+
+	<p style="color:white" align="center">aaaaaaaaaaaaaaaa&nbsp;&nbsp;&nbsp;&nbsp;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+	<hr width="50%" size="20" />
+	<pre>bbbbbbbbb
+	bbbbbbbbbbbbbbbb
+	bbbbbbbbbbbbbbbbbbbbbbb
+	bbbbbbbbbbbbbbbbb</pre>
+	jsdhfkjadhfjk<b>jsdhfkjadhfjk</b>jsdhfkjadhfjk<u>jsdhfkjadhfjk</u>jsdhfkjadhfjk<i>jsdhfkjadhfjk</i>jsdhfkjadhfjk<br />
+<!-- 20==20px? -->
+	<font face="黑体" size="2px" color="black">旧喀什的房价as解放昆仑山决定发动快速减肥快乐撒警方扣留撒酒疯绿卡</font>
+
+	<ul>one
+		<li><ul>
+			<li>1.0</li>
+			<li>1.1</li>
+			<li>1.2</li>
+		</ul></li>
+		<li>2</li>
+		<li>3</li>
+	</ul>
+<!-- that seems not to work -->
+	<ol>two
+		<ol>two-too
+			<li>almost4</li>
+			<li>almost4</li>
+			<li>almost4</li>
+		</ol>
+		<li>4</li>
+		<li>5</li>
+		<li>6</li>
+	</ol>
+<!-- act only the very outside border -->
+<table align="center" border="2" width="200" cellpadding="10" cellspacing="10" bgcolor="#cccccc" bordercolor="yellow">
+	<caption><h3>a table</h3></caption>
+	<tr>
+		<th>11</th>
+		<th>12</th>
+		<td>13</td>
+		<td>14</td>
+	</tr>
+	<tr>
+		<td>21</td>
+		<td>22</td>
+		<td>23</td>
+	</tr>
+	<tr>
+		<td>31</td>
+		<td>32</td>
+		<td>33</td>
+	</tr>
+	<tr>
+		<td>41</td>
+		<td>42</td>
+		<td>43</td>
+	</tr>
+</table>
+
+</body>
+</html>
