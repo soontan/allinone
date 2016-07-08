c7861a42c4d3067bbd154dae0d6878b958f12a58
soontan <luandsun@foxmail.com>
2016-07-08 16:15:52 +0800

    Create day21:coloursforgames.php

---
+++ day21:coloursforgames.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+  	<meta content="width=device-width,user-scalable=no" name="viewport">
+  	<head>
+    <title>几个游戏配色</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <style type="text/css">
+body {
+background-color:#DCDCDC;
+}
+
+div {
+float:left;
+margin-top:5px;
+}
+
+.cr {
+background-color:#6959cd;
+width:50%;
+text-align:center;
+padding-top:70px;
+padding-bottom:70px;
+min-width:250px;
+}
+
+.hs {
+background-color:#a0522d;
+width:50%;
+text-align:center;
+padding-top:70px;
+padding-bottom:70px;
+min-width:250px;
+}
+
+.wow {
+background-color:#548b54;
+width:50%;
+text-align:center;
+padding-top:70px;
+padding-bottom:70px;
+min-width:250px;
+}
+
+.ow {
+background-color:#808080;
+width:50%;
+text-align:center;
+padding-top:70px;
+padding-bottom:70px;
+min-width:250px;
+}
+
+.cr a {
+text-decoration:none;
+font-family:Microsoft Yahei,sans-serif;
+font-size:30px;
+color:#00bfff;
+font-weight:700;
+}
+
+.hs a {
+text-decoration:none;
+font-family:Microsoft Yahei,sans-serif;
+font-size:30px;
+color:#8b2500;
+font-weight:700;
+}
+
+.wow a {
+text-decoration:none;
+font-family:Microsoft Yahei,sans-serif;
+font-size:30px;
+color:#2f4f4f;
+font-weight:700;
+}
+
+.ow a {
+text-decoration:none;
+font-family:Microsoft Yahei,sans-serif;
+font-size:30px;
+color:#FFFF00;
+font-weight:700;
+}
+
+.main,.subcon{
+    width:100%;
+}
+
+.lol {
+background-color:#27408B;
+width:50%;
+text-align:center;
+padding-top:70px;
+padding-bottom:70px;
+min-width:250px;
+}
+
+.lol a {
+text-decoration:none;
+font-family:Microsoft Yahei,sans-serif;
+font-size:30px;
+color:#DAA520;
+font-weight:700;
+}
+
+.dota {
+background-color:#1E1E1E;
+width:50%;
+text-align:center;
+padding-top:70px;
+padding-bottom:70px;
+min-width:250px;
+}
+
+.dota a {
+text-decoration:none;
+font-family:Microsoft Yahei,sans-serif;
+font-size:30px;
+color:#CD2626;
+font-weight:700;
+}
+
+a:hover{color:#FFFFFF;}
+    </style>
+  </head>
+  <body>
+    <div class="main">
+        <div class="subcon">
+          <div class="cr">
+            <a href="http://icugame.com/cr">皇室战争</a>
+          </div>
+        </div>
+        <div class="subcon">
+          <div class="hs">
+            <a href="http://icugame.com/hs">炉石传说</a>
+          </div>
+        </div>
+        <div class="subcon">
+          <div class="wow">
+            <a href="http://icugame.com/wow">魔兽世界</a>
+          </div>
+        </div>
+        <div class="subcon">
+          <div class="ow">
+            <a href="http://icugame.com/ow">守望先锋</a>
+          </div>
+        </div>
+        <div class="subcon">
+          <div class="lol">
+            <a href="http://icugame.com/lol">英雄联盟</a>
+          </div>
+        </div>
+        <div class="subcon">
+          <div class="dota">
+            <a href="http://icugame.com/dota">DOTA2</a>
+          </div>
+        </div>
+    </div>
+  </body>
+</html>
